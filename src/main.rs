010a865c48f6893670213b72cc6f6ccdd22a3649
fe23004 <fe23004@ues.edu.sv>
2026-06-14 22:49:09 -0600

    integración de  exportación a formato DOT (Graphviz) y pruebas finales del sistema

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 pub mod modelo;
 pub mod recorrido;
 pub mod ruta;
-
+use petgraph::dot::{Dot, Config};
 //use petgraph::dot::{Dot, Config};
 
 fn main() {
@@ -27,3 +27,28 @@ fn main() {
     println!("│  Rutas (aristas):    {:>3}                │", grafo.edge_count());
     println!("└─────────────────────────────────────────┘");
     println!();
+
+    // Ejecución de rutas de prueba (Modificado para usar índices directamente)
+    // El 0 corresponde a Berlín (el primer nodo agregado)
+    
+
+    recorrido::ejecutar_bfs_nativo(&grafo, petgraph::graph::NodeIndex::new(0));
+    
+    println!("═══════════════════════════════════════════");
+    println!("  RUTA CON MENOS ESCALAS: Toronto → Sydney");
+    println!("═══════════════════════════════════════════");
+    // El 4 es Toronto y el 3 es Sydney
+    ruta::bfs_ruta(&grafo, petgraph::graph::NodeIndex::new(4), petgraph::graph::NodeIndex::new(3));
+    println!();
+
+    println!("═══════════════════════════════════════════");
+    println!("  RUTA CON MENOS ESCALAS: Berlin → Sydney");
+    println!("═══════════════════════════════════════════");
+    // El 0 es Berlin y el 3 es Sydney
+    ruta::bfs_ruta(&grafo, petgraph::graph::NodeIndex::new(0), petgraph::graph::NodeIndex::new(3));
+    println!();
+
+    let dot_output = Dot::with_config(&grafo, &[Config::EdgeNoLabel]);
+    println!("{:?}", dot_output);
+}
+
